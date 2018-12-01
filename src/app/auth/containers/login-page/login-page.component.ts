5fea4bc005b21c7655a7aac64e0c215104b9e791
okkindel <ebrithil97@gmail.com>
2018-12-01 07:30:13 +0100

    new themes

--- src/app/auth/containers/login-page/login-page.component.ts
+++ src/app/auth/containers/login-page/login-page.component.ts
@@ -1,31 +1,37 @@
 import * as AuthActions from '../../../state/auth/actions/';
 import { SnackbarService } from '../../../shared/services';
 import * as fromAuth from '../../../state/auth/reducers/';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store, select, compose } from '@ngrx/store';
 import { ICredentials } from '../../models';
 import { filter } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { isNullOrUndefined, isUndefined } from 'util';
+import * as fromThemes from '../../../state/themes/reducers';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
 
   errorMessage$: Observable<any>;
   isPending$: Observable<boolean>;
+  theme: String;
 
   constructor(
     private store: Store<fromAuth.IState>,
-    private snackBar: SnackbarService) {
+    private snackBar: SnackbarService,
+    private themes: Store<fromThemes.IState>) {
     this.errorMessage$ = store.pipe(
       select(fromAuth.getError),
       filter(err => err !== undefined)
     );
     this.isPending$ = store.pipe(select(fromAuth.getIsPending));
+  }
+
+  ngOnInit(): void {
+    this.themes.pipe(select(fromThemes.getTheme)).subscribe(res => this.theme = res);
   }
 
   login(credentials: ICredentials) {
